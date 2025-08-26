bf28deb5a25b4a9f28a68c738c2cc515d9bde1f0
Rafael <115298880+RafaelLourenco2004@users.noreply.github.com>
2025-08-26 22:38:47 +0000

    ex2

--- src/ex3_contador.c
+++ src/ex3_contador.c
@@ -40,7 +40,8 @@ int main() {
      * TODO 1: Implementar loop de leitura
      * Loop até read() retornar 0 (fim do arquivo)
      */
-    while (/* TODO: condição do loop */) {
+    bytes_lidos = read(fd, buffer, BUFFER_SIZE - 1);
+    while (bytes_lidos > 0) {
         total_reads++;
         
         /*
@@ -48,22 +49,27 @@ int main() {
          */
         for (int i = 0; i < bytes_lidos; i++) {
             /* TODO: verificar '\n' e incrementar total_linhas */
+            if (buffer[i] == '\n')
+                total_linhas++;
         }
         
         /*
          * TODO 3: Somar total de caracteres
          */
+        total_caracteres += bytes_lidos;
+
         /* TODO: total_caracteres += ... */;
         
         if (total_reads % 10 == 0) {
             printf("Processadas %d chamadas read()...\n", total_reads);
         }
+        bytes_lidos = read(fd, buffer, BUFFER_SIZE - 1);
     }
     
     /*
      * TODO 4: Verificar se houve erro na leitura
      */
-    if (/* TODO: condição de erro */) {
+    if (status == -1) {
         perror("Erro na leitura");
         close(fd);
         return 1;
